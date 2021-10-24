2773fa01241fa19f6e5acc70966abf10dfc08279
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-24 10:14:14 +0300

    Login Activity Added

---
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.nyaranga_social_app_version2.controller.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
