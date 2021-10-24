bdcc56b94d71fd6d3fce3317b9d3db7a4ec89473
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-24 21:10:33 +0300

    Declared the variables in the create account section

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/CreateAccountActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.moringaschool.nyaranga_social_app_version2.R;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    @BindView(R.id.createUserButton) Button mCreateUserButton;
+    @BindView(R.id.nameEditText) EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
